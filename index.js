f4c097fbb29ad35d8e0250dbd0835a027cf49e76
Kel Den <keldenong@gmail.com>
2020-09-18 17:17:14 +0800

    Added body parser

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let express = required('express');
 let app = express();
+let bodyParser = required('body-parser');
 let cors = required('cors');
 
 app.use(cors());
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', function (req, res) {
   res.send('Hello World! Second Attempt');
