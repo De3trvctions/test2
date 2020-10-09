3c37aaad225d1582695007e436693db82e347cb3
Kel Den <keldenong@gmail.com>
2020-10-09 14:39:43 +0800

    adding error to the code

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({
+    info: 'Node.js, Express, and Postgres API'
+  })
 })
 
 app.get('/users', db.getUsers)
 app.get('/users/id:id', db.getUserById)
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+      console.log(`App running on port ${port}.`)
